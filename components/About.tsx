1f0c9b414d50b8c086ac0f913e06930b2bf54acb
eurelR15 <116075531+eurelR15@users.noreply.github.com>
2022-11-07 18:11:06 +0200

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ export default function About({}:Props) {
       className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-16 uppercase tracking-[25px] text-gray-500 text-2xl'>About</h3>
 
-        <motion.img
+        <motion.image
         initial={{x:-200,}}
         transition={{duration:1.2}}
         whileInView={{opacity:1,x:0}}
